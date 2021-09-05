10cfd929d59d221199ffc0a806ab4be76d49b057
Denis Panevin <d.panevin@gmail.com>
2021-09-05 17:25:00 +0300

    Исправляет мелкие недоработки

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,19 @@
-import HomePage from 'components/Home/Home';
-import Nav from 'components/Nav/Nav';
-import Phonebook from 'components/Phonebook/Phonebook';
-import Login from 'components/Authorization/Login';
-import Register from 'components/Authorization/Register';
 import { Switch } from 'react-router-dom';
 import { PrivateRoute } from './PrivateRoute';
 import { PublicRoute } from './PublicRoute';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectIsFetchingCurrentUser, selectToken } from 'redux/Auth/authSelectors';
-import { useEffect } from 'react';
+import { lazy, useEffect } from 'react';
 import authOperations from 'redux/Auth/authOperation';
+const HomePage = lazy(() => import('components/Home/Home' /* webpackChunkName: 'HomePage' */));
+const Nav = lazy(() => import('components/Nav/Nav' /* webpackChunkName: 'Nav' */));
+const Phonebook = lazy(() =>
+  import('components/Phonebook/Phonebook' /* webpackChunkName: 'Phonebook' */),
+);
+const Login = lazy(() => import('components/Authorization/Login' /* webpackChunkName: 'Login' */));
+const Register = lazy(() =>
+  import('components/Authorization/Register' /* webpackChunkName: 'Register' */),
+);
 
 export function App() {
   const token = useSelector(selectToken);
